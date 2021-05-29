ddf34ef2c5e4e285e0ee0a92349344e476082b31
AxSch <alex_schuneman@outlook.com>
2021-05-29 17:31:51 +0100

    Add Search component to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Search from './pages/Search/Search';
 
 function App() {
   return (
     <div className="App">
-      
+      <Search />
     </div>
   );
 }
