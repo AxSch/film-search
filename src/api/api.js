76f18e0877b96a583db92b0f1550fa495d8bb3e3
AxSch <alex_schuneman@outlook.com>
2021-05-30 16:17:03 +0100

    Refactor & add credits fn's to for each category

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
-const API_KEY = process.env.API_KEY
+
 
 const fetchMovies = async (query) => {
     try {
-        const movies = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&include_adult=false&query=${query}`)
+        const movies = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=76e77a494aebd05ce5c4f5000c61cc0e&include_adult=false&query=${query}`)
         const data = await movies.json()
         return data
     } catch (error) {
@@ -12,7 +12,7 @@ const fetchMovies = async (query) => {
 
 const fetchTVshows = async (query) => {
     try {
-        const tvShows = await fetch(`https://api.themoviedb.org/3/search/tv?api_key=${API_KEY}&include_adult=false&query=${query}`)
+        const tvShows = await fetch(`https://api.themoviedb.org/3/search/tv?api_key=76e77a494aebd05ce5c4f5000c61cc0e&include_adult=false&query=${query}`)
         const data = await tvShows.json()
         return data
     } catch (error) {
@@ -22,7 +22,7 @@ const fetchTVshows = async (query) => {
 
 const fetchPeople = async (query) => {
     try {
-        const people = await fetch(`https://api.themoviedb.org/3/search/person?api_key=${API_KEY}&include_adult=false&query=${query}`)
+        const people = await fetch(`https://api.themoviedb.org/3/search/person?api_key=76e77a494aebd05ce5c4f5000c61cc0e&include_adult=false&query=${query}`)
         const data = await people.json()
         return data
     } catch (error) {
@@ -30,4 +30,34 @@ const fetchPeople = async (query) => {
     }
 }
 
-export { fetchMovies,  fetchPeople, fetchTVshows }
+const fetchTVCredits = async (query) => {
+    try {
+        const tvCredits = await fetch(`https://api.themoviedb.org/3/tv/${query}/credits?api_key=76e77a494aebd05ce5c4f5000c61cc0e`)
+        const data = await tvCredits.json()
+        return data
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+const fetchMovieCredits = async (query) => {
+    try {
+        const movieCredits = await fetch(`https://api.themoviedb.org/3/movie/${query}/credits?api_key=76e77a494aebd05ce5c4f5000c61cc0e`)
+        const data = await movieCredits.json()
+        return data
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+const fetchPersonCredits = async (query) => {
+    try {
+        const movieCredits = await fetch(`https://api.themoviedb.org/3/person/${query}/combined_credits?api_key=76e77a494aebd05ce5c4f5000c61cc0e`)
+        const data = await movieCredits.json()
+        return data
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+export { fetchMovies,  fetchPeople, fetchTVshows, fetchTVCredits, fetchMovieCredits, fetchPersonCredits }
