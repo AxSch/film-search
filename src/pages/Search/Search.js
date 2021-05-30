d10227806c27684974f09eb1dc8e157bb9e5ee75
AxSch <alex_schuneman@outlook.com>
2021-05-30 13:00:49 +0100

    Refactor functions & add qs along with history

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,24 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import { fetchMovies, fetchTVshows, fetchPeople } from '../../api/api'
 import ResultsList from '../../components/ResultsList/ResultsList';
+import { useHistory } from "react-router-dom";
 
 const Search = () => {
     const [searchData, setSearchData] = useState({})
     const [isDropdown, setIsDropdown] = useState(false)
+    const [searchQuery, setSearchQuery] = useState('')
+    const history = useHistory()
     
-    const handleSearch = async (value) => {
+    const fetchSearchResults = async (value) => {
+        const peopleResults = await fetchPeople(value)
+        const tvShowResults = await fetchTVshows(value)
+        const movieResults = await fetchMovies(value)
+        setSearchData({people: peopleResults.results, tvShows: tvShowResults.results, movies: movieResults.results})
+    }
+
+    const handleSearch = (value) => {
         if (value.length >= 5) {
-            const peopleResults = await fetchPeople(value)
-            const tvShowResults = await fetchTVshows(value)
-            const movieResults = await fetchMovies(value)
-            setSearchData({people: peopleResults.results, tvShows: tvShowResults.results, movies: movieResults.results})
+            fetchSearchResults(value)
             setIsDropdown(true)
+            setSearchQuery(value)
         }
+        setSearchQuery(value)
     }
 
     const onSubmit = (e) => {
         e.preventDefault();
         setIsDropdown(false)
+        const qs = new URLSearchParams({item: searchQuery}).toString()
+        if (Object.keys(searchData).length < 1) {
+            fetchSearchResults(searchQuery)
+        }
+        history.push({
+            pathname: "/search",
+            search: "?" + qs
+        })
     }
 
     const renderResultsList = () => {
