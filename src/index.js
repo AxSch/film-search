42d17dbac06f4f1a4ab1f0f5690e3252ec4db3cf
AxSch <alex_schuneman@outlook.com>
2021-05-30 16:15:35 +0100

    Setup store & persistGate

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as dotenv from 'dotenv'
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from './store/store';
+import dotenv from 'dotenv'
 
 dotenv.config()
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
