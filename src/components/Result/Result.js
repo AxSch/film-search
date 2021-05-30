f4b9561bf7c91a19504ba5158f9314254daa1a70
AxSch <alex_schuneman@outlook.com>
2021-05-30 16:18:31 +0100

    Refactor to use react-router-dom Link

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Result = ({ resultData }) => {
+const Result = ({ resultData, category }) => {
     return (
         <li>
-            <span>{resultData.name}</span>
-            <span>{resultData.known_for_department}</span>
+            <Link to={`/item/${resultData.id}&category=${category}`}>
+                <span>{resultData.name}</span>
+                <span>{resultData.known_for_department}</span>
+            </Link>
         </li>
     )
 }
