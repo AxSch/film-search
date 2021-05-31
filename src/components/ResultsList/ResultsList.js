2c17a198a4acc88eae730bc03ee256939cb58f21
AxSch <alex_schuneman@outlook.com>
2021-05-31 22:26:02 +0100

    Refactor to close dropdown & setCurrentItem

--- src/components/ResultsList/ResultsList.js
+++ src/components/ResultsList/ResultsList.js
@@ -1,27 +1,48 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { setCurrentItem } from '../../reducers/searchResults/searchResultSlice'
 import Result from '../Result/Result'
 
-const ResultsList = ({ results }) => {
+const ResultsList = ({ results, handleDropdown }) => {
+    const dispatch = useDispatch()
+
+    const handleOnClick = (result) => {
+        handleDropdown(false)
+        dispatch(setCurrentItem(result))
+    }
+
     const renderResults = () => {
         return (
             <>
                 <span>Tv Shows</span>
                 {results['tvShows'].map((tvShow, index) => {
-                    return <Result category="tvShows" resultData={tvShow} key={index} />
+                    return (
+                        <div onClick={() => handleOnClick(tvShow)} key={index}>
+                            <Result category="tvShows" resultData={tvShow} />
+                        </div>
+                    )
                 })}
 
                 <div />
 
                 <span>Movies</span>
                 {results['movies'].map((movie, index) => {
-                    return <Result category="movies" resultData={movie} key={index} />
+                    return (
+                        <div onClick={() => handleOnClick(movie)} key={index}>
+                            <Result category="movies" resultData={movie} />
+                        </div>
+                    )
                 })}
 
                 <div />
 
                 <span>Actor/Actress</span>
                 {results['people'].map((person, index) => {
-                    return <Result category="people" resultData={person} key={index} />
+                    return (
+                        <div onClick={() => handleOnClick(person)} key={index}>
+                            <Result category="people" resultData={person} />
+                        </div>
+                    )
                 })}
             </>
         )
