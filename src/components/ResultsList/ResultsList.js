1bd14a47a4aae2c290a9a7c693030e468fa61e6b
AxSch <alex_schuneman@outlook.com>
2021-06-01 00:53:06 +0100

    Add renderFn()'s & filter prop

--- src/components/ResultsList/ResultsList.js
+++ src/components/ResultsList/ResultsList.js
@@ -3,7 +3,7 @@ import { useDispatch } from 'react-redux'
 import { setCurrentItem } from '../../reducers/searchResults/searchResultSlice'
 import Result from '../Result/Result'
 
-const ResultsList = ({ results, handleDropdown }) => {
+const ResultsList = ({ results, handleDropdown, filter }) => {
     const dispatch = useDispatch()
 
     const handleOnClick = (result) => {
@@ -11,39 +11,63 @@ const ResultsList = ({ results, handleDropdown }) => {
         dispatch(setCurrentItem(result))
     }
 
+    const renderTVshows = (results, filter) => {
+        if (filter === 'all' || filter === 'tvShows') {
+            return (
+                <div>
+                    <span>Tv Shows</span>
+                    {results['tvShows'].map((tvShow, index) => {
+                        return (
+                            <div onClick={() => handleOnClick(tvShow)} key={index}>
+                                <Result category="tvShows" resultData={tvShow} />
+                            </div>
+                        )
+                    })}
+                </div>
+            )
+        }
+    }
+
+    const renderMovies = (results, filter) => {
+        if (filter === 'all' || filter === 'movies') {
+            return (
+                <div>
+                    <span>Movies</span>
+                    {results['movies'].map((movie, index) => {
+                        return (
+                            <div onClick={() => handleOnClick(movie)} key={index}>
+                                <Result category="movies" resultData={movie} />
+                            </div>
+                        )
+                    })}
+                </div>
+            )
+        }
+    }
+
+    const renderPeople = (results, filter) => {
+        if (filter === 'all' || filter === 'people') {
+            return (
+                <div>
+                    <span>People</span>
+                    {results['people'].map((people, index) => {
+                        return (
+                            <div onClick={() => handleOnClick(people)} key={index}>
+                                <Result category="people" resultData={people} />
+                            </div>
+                        )
+                    })}
+                </div>
+            )
+        }
+    }
+
     const renderResults = () => {
         return (
             <>
-                <span>Tv Shows</span>
-                {results['tvShows'].map((tvShow, index) => {
-                    return (
-                        <div onClick={() => handleOnClick(tvShow)} key={index}>
-                            <Result category="tvShows" resultData={tvShow} />
-                        </div>
-                    )
-                })}
-
-                <div />
-
-                <span>Movies</span>
-                {results['movies'].map((movie, index) => {
-                    return (
-                        <div onClick={() => handleOnClick(movie)} key={index}>
-                            <Result category="movies" resultData={movie} />
-                        </div>
-                    )
-                })}
-
-                <div />
-
-                <span>Actor/Actress</span>
-                {results['people'].map((person, index) => {
-                    return (
-                        <div onClick={() => handleOnClick(person)} key={index}>
-                            <Result category="people" resultData={person} />
-                        </div>
-                    )
-                })}
+                {renderTVshows(results, filter)}
+                {renderMovies(results, filter)}
+                {renderPeople(results, filter)}
             </>
         )
     }
