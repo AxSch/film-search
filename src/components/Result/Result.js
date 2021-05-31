f338ed2ee0a7ca4674b424aada047c56b037f435
AxSch <alex_schuneman@outlook.com>
2021-05-31 22:24:44 +0100

    Refactor to handle movies

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -2,13 +2,32 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Result = ({ resultData, category }) => {
+
+    const handleCategory = () => {
+        if (category === 'movies') {
+            return (
+                <li>
+                    <Link to={`/item/${resultData.id}&category=${category}`}>
+                        <span>{resultData.title}</span>
+                    </Link>
+                </li>
+            )
+        } else {
+            return (
+                <li>
+                    <Link to={`/item/${resultData.id}&category=${category}`}>
+                        <span>{resultData.name}</span>
+                        <span>{resultData.known_for_department}</span>
+                    </Link>
+                </li>
+            )
+        }
+    }
+
     return (
-        <li>
-            <Link to={`/item/${resultData.id}&category=${category}`}>
-                <span>{resultData.name}</span>
-                <span>{resultData.known_for_department}</span>
-            </Link>
-        </li>
+        <>
+            {handleCategory()}
+        </>
     )
 }
 
