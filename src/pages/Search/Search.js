a4e547969d9c7103aea1af0b74e70e38052c86f6
AxSch <alex_schuneman@outlook.com>
2021-06-01 00:54:40 +0100

    Add filters
    Also added handleCheckbox(), extra state props & checkbox markup

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory, useLocation } from "react-router-dom";
 import { useDispatch } from 'react-redux';
 import { fetchMovies, fetchTVshows, fetchPeople } from '../../api/api'
@@ -9,6 +9,8 @@ const Search = () => {
     const [searchData, setSearchData] = useState({})
     const [isDropdown, setIsDropdown] = useState(false)
     const [searchQuery, setSearchQuery] = useState('')
+    const [filter, setFilter] = useState('all')
+    const [isSelected, setIsSelected] = useState(true)
     const history = useHistory()
     const dispatch = useDispatch()
     let location = useLocation()
@@ -19,7 +21,7 @@ const Search = () => {
         const movieResults = await fetchMovies(value)
         setSearchData({ people: peopleResults.results, tvShows: tvShowResults.results, movies: movieResults.results })
     }
-    
+
     const handleSearch = (value) => {
         if (value.length >= 5) {
             fetchSearchResults(value)
@@ -28,7 +30,16 @@ const Search = () => {
         }
         setSearchQuery(value)
     }
-    
+
+    const handleCheckbox = (value) => {
+        setFilter(value)
+        if (filter !== 'all') {
+            setIsSelected(false)
+        } else {
+            setIsSelected(true)
+        }
+    }
+
     const onSubmit = (e) => {
         e.preventDefault();
         setIsDropdown(false)
@@ -53,12 +64,14 @@ const Search = () => {
 
     const renderResultsList = () => {
         if (!isDropdown && Object.values(searchData)[0] !== undefined && location.search !== "") {
-            return (
-                <div>
-                    <h3>Results</h3>
-                    <ResultsList results={searchData} handleDropdown={setIsDropdown} />
-                </div>
-            )
+            if (filter !== '') {
+                return (
+                    <div>
+                        <h3>Results</h3>
+                        <ResultsList results={searchData} handleDropdown={setIsDropdown} filter={filter} />
+                    </div>
+                )
+            }
         } else if (searchQuery === '') {
             return (
                 <div>
@@ -78,8 +91,18 @@ const Search = () => {
                 <input type="search" id="search" onChange={e => handleSearch(e.target.value)} />
                 <input type="submit" value="Submit" />
                 {isDropdown && Object.keys(searchData).length > 0 && searchQuery !== '' ?
-                    <ResultsList results={searchData} handleDropdown={setIsDropdown} /> : null
+                    <ResultsList results={searchData} handleDropdown={setIsDropdown} filter={filter}/> : null
                 }
+                <div>
+                    <input type="radio" id="movies" onChange={(event) => handleCheckbox(event.target.value)} name="cinematography" value="movies"></input>
+                    <label htmlFor="movies">Movies</label><br></br>
+                    <input type="radio" id="tvShows" onChange={(event) => handleCheckbox(event.target.value)} name="cinematography" value="tvShows"></input>
+                    <label htmlFor="tvShows">TV Shows</label><br></br>
+                    <input type="radio" id="people" onChange={(event) => handleCheckbox(event.target.value)} name="cinematography" value="people"></input>
+                    <label htmlFor="people">People</label><br></br>
+                    <input type="radio" checked={isSelected} id="all" onChange={(event) => handleCheckbox(event.target.value)} name="cinematography" value="all"></input>
+                    <label htmlFor="all">All</label><br></br>
+                </div>
             </form>
             {renderResultsList()}
         </div>
