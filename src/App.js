24486a7e36e4b7ae14f42f24a717e17b9cb96b58
AxSch <alex_schuneman@outlook.com>
2021-06-01 17:04:26 +0100

    Utilise error boundary around Details route

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import './App.css';
 import Search from './pages/Search/Search';
 import { BrowserRouter, Route } from 'react-router-dom'
 import Details from './pages/Details/Details';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 
 function App() {
   return (
     <BrowserRouter>
         <Search />
-        <Route
-          exact
-          path="/item/:id"
-          component={Details}
-        />
-      </BrowserRouter>
+        <ErrorBoundary>
+          <Route
+            exact
+            path="/item/:id"
+            component={Details}
+          />
+        </ErrorBoundary>
+
+        </BrowserRouter>
   );
 }
 
