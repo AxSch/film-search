cabe96dee98be0291d9f471914625c9d2a8586d7
AxSch <alex_schuneman@outlook.com>
2021-06-02 17:12:31 +0100

    Add renderConnected()

--- src/utils/testHelpers.js
+++ src/utils/testHelpers.js
@@ -1,8 +1,32 @@
 import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom'
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import searchResultReducer from '../reducers/searchResults/searchResultSlice';
 
 export const renderWithRouter = (ui, { route = '/' } = {}) => {
     window.history.pushState({}, 'Test page', route)
   
     return render(ui, { wrapper: BrowserRouter })
   }
+
+
+export const renderConnected = (
+  ui, {
+    store = createStore(searchResultReducer, {}),
+    route = '/',
+    ...renderOptions
+  } = {}
+) => {
+  window.history.pushState({}, 'Test page', route)
+  const Wrapper = ({ children }) => (
+    <Provider store={store}>
+      <BrowserRouter>
+        {children}
+      </BrowserRouter>
+    </Provider>
+  );
+  return render(ui, { wrapper: Wrapper, ...renderOptions});
+};
+
+export default renderConnected;
